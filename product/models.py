d3baea8ffa68ad2db24dc00c9404404e7a502b33
Yazid <yazeedkurdi@hotmail.com>
2023-07-17 23:27:42 +0300

    add desc field

--- product/models.py
+++ product/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     brand = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
     min_price = models.DecimalField(max_digits=10, decimal_places=2)
     max_price = models.DecimalField(max_digits=10, decimal_places=2)
     min_quantity = models.IntegerField()
