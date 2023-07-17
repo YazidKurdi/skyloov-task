34c76ebadd61b187718f7800ff239cc0aa89d680
Yazid <yazeedkurdi@hotmail.com>
2023-07-17 19:03:59 +0300

    init products model

---
+++ product/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Product(models.Model):
+
+    CATEGORY_CHOICES = (
+        ('residential', 'Residential'),
+        ('commercial', 'Commercial'),
+        ('mixed_use', 'Mixed Use'),
+    )
+
+    RATING_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    brand = models.CharField(max_length=100)
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_price = models.DecimalField(max_digits=10, decimal_places=2)
+    min_quantity = models.IntegerField()
+    max_quantity = models.IntegerField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
