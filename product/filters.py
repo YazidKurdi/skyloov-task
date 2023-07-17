3f666e2e6592861cc9b856701176289ae2aa05c9
Yazid <yazeedkurdi@hotmail.com>
2023-07-17 19:03:37 +0300

    init filterset query to filter products

---
+++ product/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+from .models import Product
+
+
+
+class ProductFilter(django_filters.FilterSet):
+
+
+    min_price = django_filters.NumberFilter(field_name='min_price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='max_price', lookup_expr='lte')
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+
+    class Meta:
+        model = Product
+        fields = {
+            'category': ["in", "exact"],
+        }
