512a40b7a1e9bb83d5ba708b5dffbf7f9c4ce95f
Yazid <yazeedkurdi@hotmail.com>
2023-07-18 15:41:24 +0300

    comment out product urls

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
+    # path('products/', views.ProductList.as_view()),
     path('product-detail/', views.ProductListDetail.as_view(), name='product-filter'),
 ]
