98918b2b5aa242f591f20f70a6a44c7afb5169ce
Yazid <yazeedkurdi@hotmail.com>
2023-07-19 18:27:01 +0300

    login documentation helper module

---
+++ skyloov/token.py
@@ -0,0 +1,24 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+class TokenObtainPairView(TokenObtainPairView):
+
+    @swagger_auto_schema(
+        operation_summary="Retrieve access and refresh tokens",
+        operation_description="This endpoint retrieves access and refresh tokens when the user logs in.",
+        tags=["Login"],
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class TokenRefreshView(TokenRefreshView):
+
+    @swagger_auto_schema(
+        operation_summary="Retrieve access token",
+        operation_description="This endpoint retrieves access token when a refresh token is supplied.",
+        tags=["Login"],
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
