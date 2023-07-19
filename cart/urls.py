829ad0b5dd144db934d2da0f02fc6e00bec30cb2
Yazid <yazeedkurdi@hotmail.com>
2023-07-19 12:07:23 +0300

    init cart related url paths

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,10 @@ from cart import views
 
 urlpatterns = [
     # path('products/', views.ProductList.as_view()),
-    path('add-product/<int:pk>/', views.AddRemoveProductCart.as_view(), name='add-product'),
+    # path('add-product/<int:pk>/', views.AddRemoveProductCart.as_view(), name='add-product'),
+    path('add-product/<int:pk>/', views.AddProductCart.as_view(), name='add-product'),
+    path('delete-product/', views.DeleteProductCart.as_view(), name='delete-product'),
+    path('update-product/', views.UpdateProductCart.as_view(), name='update-product'),
+    path('all-products/', views.RetrieveCart.as_view(), name='all-products'),
 ]
+
