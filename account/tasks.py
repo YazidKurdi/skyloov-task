24f93c8ae495312b7e3d4ce93d466852e4cb9322
Yazid <yazeedkurdi@hotmail.com>
2023-07-20 16:08:48 +0300

    init celery email task

---
+++ account/tasks.py
@@ -0,0 +1,13 @@
+
+from celery import shared_task
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+
+@shared_task
+def send_welcome_email_task(user_email):
+
+    subject = 'Welcome to Skyloov'
+    message = 'Thank you for joining Skyloov!'
+    from_email = 'yazid.kurdi@gmail.com'
+    recipient_list = [user_email]
+    send_mail(subject, message, from_email, recipient_list)
