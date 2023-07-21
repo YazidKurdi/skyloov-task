48196b5efece39824f5d2a3aa2e3c4d94a659b07
Yazid <yazeedkurdi@hotmail.com>
2023-07-22 00:24:56 +0300

    remove comments & unwanted imports

--- product/views.py
+++ product/views.py
@@ -1,53 +1,12 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status
-from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.response import Response
-# from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-# from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters import rest_framework as filters
 
 from . import SearchPagination
 from .filters import ProductFilter
 from .models import Product
 from .serializers import ProductSerializer
-
-
-
-
-# class ProductList(APIView):
-#
-#     # authentication_classes = [TokenAuthentication]
-#     # permission_classes = [IsAuthenticated]
-#
-#
-#     def get(self, request):
-#         """
-#         API view to retrieve a single product
-#         """
-#         products = Product.objects.all()
-#         serializer = ProductSerializer(products, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# class ProductListDetail(APIView):
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAuthenticated]
-#     filter_backends = [OrderingFilter]  # Add the OrderingFilter backend
-#
-#     def get(self, request):
-#         queryset = Product.objects.all()
-#         filter_class = ProductFilter(request.query_params, queryset=queryset)
-#
-#         # Apply the filtering and ordering
-#         filtered_queryset = filter_class.qs
-#         ordered_queryset = self.filter_queryset(filtered_queryset)
-#
-#         serializer = ProductSerializer(ordered_queryset, many=True)
-#         return Response(serializer.data)
-
 
 class ProductList(generics.ListCreateAPIView):
     authentication_classes = []
