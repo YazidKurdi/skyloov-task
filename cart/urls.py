e0c75a0140c7d4ec5aa634c108e09c28e6b860d0
Yazid <yazeedkurdi@hotmail.com>
2023-07-22 12:35:02 +0300

    add-product no longer accept id in URL, expected to be in request body

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from cart import views
 
 urlpatterns = [
-    path('add-product/<int:pk>/', views.AddProductCart.as_view(), name='add-product'),
+    path('add-product/', views.AddProductCart.as_view(), name='add-product'),
     path('delete-product/', views.DeleteCartItem.as_view(), name='delete-product'),
     path('update-product/', views.UpdateCartItem.as_view(), name='update-product'),
     path('all-products/', views.RetrieveCart.as_view(), name='all-products'),
